8d13d9a53e3cb138bfdf681e278545e5664298d9
Szymon <szymon.osinski.2009@gmail.com>
2026-01-06 12:31:09 +0100

    feat: Jwt Authentication Filter

--- src/main/java/com/example/miro/security/SecurityConfiguration.java
+++ src/main/java/com/example/miro/security/SecurityConfiguration.java
@@ -1,15 +1,21 @@
 package com.example.miro.security;
 
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@AllArgsConstructor
 public class SecurityConfiguration {
+  private final JwtAuthenticationFilter jwtAuthenticationFilter;
+  private final AuthenticationProvider authenticationProvider;
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http
@@ -21,10 +27,13 @@ public class SecurityConfiguration {
                 "/swagger-ui/**",
                 "/swagger-ui.html"
             ).permitAll()
+            .anyRequest().authenticated()
         )
         .sessionManagement(session -> session
             .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-        );
+        )
+        .authenticationProvider(authenticationProvider)
+        .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
     return http.build();
   }
